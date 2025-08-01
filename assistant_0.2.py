ae09d5a6dab0230a3302d763831d6603ffce3a0d
zaurdar <163637879+zaurdar@users.noreply.github.com>
2025-08-01 13:46:35 +0200

    add

--- assistant_0.2.py
+++ assistant_0.2.py
@@ -6,7 +6,8 @@ import joblib
 from sklearn.tree import plot_tree
 import matplotlib.pyplot as plt
 import json
-
+pw = 19
+fw = 1
 def degres_heure_glissants(temperatures, t_base=15.0):
     dh_glissant = sum(t_base - t for t in temperatures)
     return dh_glissant
@@ -18,41 +19,45 @@ def predict(X,clf):
     return y_pred
 
 clf = joblib.load("./modele_multioutput.pkl")
-#import data
-test = torch.load('C:/Users/hugom/OneDrive/Documents/Stage_2025/dev_Cnn/dataset/temp.pt',weights_only=False)
-test_past = torch.load('C:/Users/hugom/OneDrive/Documents/Stage_2025/dev_Cnn/dataset/temp_past.pt',weights_only=False)
 #import fichier json
 with open("./data/trends.json", "r") as f:
     data_loaded = json.load(f)
-trends = {'occupation_list':data_loaded['occupation_list'],
-          'water consumption':data_loaded['water consumption'],
-          'electricity consumption':data_loaded['electricity consumption']}
 
 
 dj = data_loaded["dj"]
 temp = data_loaded["temp_ext"]
 data = np.array(data_loaded["commandes"])
 data = list(data.reshape(-1))
+
 #prediction creation du segment actuellement hybride pcq on a pas les conso mais il manque que ça
-X = test['input']
-X = X.reshape(-1)
-X = X.numpy()
-conso = X[240]
-X = X[261:]
-data.append(conso)
+
+conso_gaz = np.zeros(len(data_loaded["gaz consumption"])-1)
+
+for i in range(1,len(conso_gaz),1):
+    conso_gaz[i]=data_loaded["gaz consumption"][i]-data_loaded["gaz consumption"][i-1]
+
+conso_water = np.zeros(len(data_loaded["water consumption"])-1)
+for i in range(1,len(conso_water),1):
+    conso_water[i]=data_loaded["water consumption"][i]-data_loaded["water consumption"][i-1]
+t = data_loaded["time"]
+data.append(conso_gaz[pw])
 for i in temp:
     data.append(i)
-for i in X:
-    data.append(i)
-X = np.array(data)
 
+data.append(conso_water[-1])
+data.append(t)
+st.write(len(conso_gaz))
+for i in conso_gaz[pw+fw:]:
+    data.append(i)
+
+X = np.array(data)
 y_pred = predict(X,clf)
 feature_names = np.linspace(0,282,283)
 class_labels = ["Distribution__Ht_+2_Pmp2A",  "Distribution__Ht_+2_Pmp2B",  "Distribution__Ht_+27_Pmp1A",  "Distribution__Ht_+27_Pmp1B", "Distribution__Ht_-4_Pmp3A","Distribution__Ht_-4_Pmp3B","Distribution__Ht_Rad_Pmp1","Distribution__Ht_Rad_Pmp2","Production_Boiler1","Production_Boiler2","Production_Boiler3","Production_cogen"]#trends
 
-conso = np.array([test['input'][263:283]])[0]
+conso_p = np.array(conso_gaz[:pw+fw])
 
-conso_p = np.array([test_past['input'][263:283]])[0]
+conso = np.array(conso_gaz[pw+fw:])
 
 
 
@@ -67,26 +72,45 @@ c = conso[::4]#divisé par 4 pcq on a des données toutes les 15 min
 c_p = conso_p[::4]#divisé par 4 pcq on a des données toutes les 15 min
 dh = dj[int(w/4):]
 dhp = dj[:int(w/4)]
-coeffs = np.polyfit(dh, c, deg=1)
-a,b = coeffs
-y_inter = np.zeros(len(dh))
-#interpolation passsé
 
-coeffs_ = np.polyfit(dhp,c_p, deg=1)
-a_p,b_p = coeffs_
-y_p = np.zeros(len(dh))
+def mk_trend(c,c_p,x,x_p):
+    coeffs = np.polyfit(x, c, deg=1)
+    a,b = coeffs
+    y_inter = np.zeros(len(x))
+    #interpolation passsé
 
-for i in range(len(dh)):
-    y_inter[i] = a * dh[i] + b
-    y_p[i] = a_p * dhp[i] + b_p
+    coeffs_ = np.polyfit(x_p,c_p, deg=1)
+    a_p,b_p = coeffs_
+    y_p = np.zeros(len(x))
 
-df = pd.DataFrame({
+    for i in range(len(x)):
+        y_inter[i] = a * x[i] + b
+        y_p[i] = a_p * x_p[i] + b_p
+    return y_inter, y_p,a,b,a_p,b_p
+#dico data gaz
+y_inter, y_p,ag,bg,a_pg,b_pg = mk_trend(c,c_p,dh,dhp)
+d_gaz = pd.DataFrame({
     "x": dh,
     "y (données)": c,
     "y (interpolée)": y_inter,
+    "y (past données ) ": c_p,
     "y (past)":y_p
 })
+#dico data water
+conso_p = np.array(conso_water[:pw+fw])
+conso = np.array(conso_water[pw+fw:])
 
+occ_data = data_loaded["occupation_list"]
+occ_p = occ_data[pw+fw:]
+occ = occ_data[:pw+fw]
+y_inter, y_p,aw,bw,a_pw,b_pw = mk_trend(conso,conso_p,occ,occ_p)
+d_water = pd.DataFrame({
+    "x": occ[pw+fw:],
+    "y (données)": c,
+    "y (interpolée)": y_inter,
+    "y (past données ) ": c_p,
+    "y (past)":y_p
+})
 #création des labels de commandes
 cmd_labels = []
 for i in range(240):
@@ -114,35 +138,35 @@ with page1:
     with col1:
         st.subheader("trend : gaz consumption")
         tab1, tab2 = st.tabs(["Chart", "Dataframe"])
-        tab1.line_chart(df.set_index("x"), height=250)
-        tab2.dataframe(df, height=250, use_container_width=True)
-        st.write(f"trend actuelle: y = {a:.2f}x + {b:.2f}")
-        st.write(f"trend passée: y = {a_p:.2f}x + {b_p:.2f}")
-        if b > b_p:
+        tab1.line_chart(d_gaz.set_index("x"), height=250)
+        tab2.dataframe(d_gaz, height=250, use_container_width=True)
+        st.write(f"trend actuelle: y = {ag:.2f}x + {bg:.2f}")
+        st.write(f"trend passée: y = {a_pg:.2f}x + {b_pg:.2f}")
+        if bg > b_pg:
             st.write("- Possible issue with ECS")
-        if b <= b_p:
+        if bg <= b_pg:
             st.write("- Correct ECS")
-        if a > a_p:
+        if ag > a_pg:
             st.write("- Possible issue with ventilation or the air temperature")
-        if a <= a_p:
+        if ag <= a_pg:
             st.write("- Correct ventilation and air temperature")
 
     with col2:
         st.subheader("trend : water consumption")
 
         tab1, tab2 = st.tabs(["Chart", "Dataframe"])
-        tab1.line_chart(df.set_index("x"), height=250)
-        tab2.dataframe(trends, height=250, use_container_width=True)
-        st.write(f"actual trend: y = {a:.2f}x + {b:.2f}")
-        st.write(f"past trend: y = {a_p:.2f}x + {b_p:.2f}")
+        tab1.line_chart(d_water.set_index("x"), height=250)
+        tab2.dataframe(d_water, height=250, use_container_width=True)
+        st.write(f"actual trend: y = {aw:.2f}x + {bw:.2f}")
+        st.write(f"past trend: y = {a_pw:.2f}x + {b_pw:.2f}")
 
     with col3:
         st.subheader("trend : electricity consumption")
         tab1, tab2 = st.tabs(["Chart", "Dataframe"])
-        tab1.line_chart(df.set_index("x"), height=250)
-        tab2.dataframe(df, height=250, use_container_width=True)
-        st.write(f"trend actuelle: y = {a:.2f}x + {b:.2f}")
-        st.write(f"trend passée: y = {a_p:.2f}x + {b_p:.2f}")
+        tab1.line_chart(d_gaz.set_index("x"), height=250)
+        tab2.dataframe(d_gaz, height=250, use_container_width=True)
+        st.write(f"trend actuelle: y = {ag:.2f}x + {bg:.2f}")
+        st.write(f"trend passée: y = {a_pg:.2f}x + {b_pg:.2f}")
 with page2:
     st.header("Decision Trees")
     col1, col2 = st.columns(2)
---
+++ datarecup_ beta.py
@@ -0,0 +1,167 @@
+import time
+import datetime
+from datetime import timedelta
+import json
+import BAC0
+import torch
+import numpy as np
+
+def writing_trends(data):
+    print("writting...✒️")
+    for i in data:
+        print(type(i))
+    with open("./data/trends.json", "w+") as f:
+        json.dump(data, f, indent=4)
+        f.close()
+
+    print(f"{data} written to ./data/trends.json")
+
+
+bacnet = BAC0.connect('172.21.212.141')
+w = 20
+
+def get_cmds(cmds):
+    chaufferie = BAC0.device('192.168.1.17',2886551,bacnet) #RPC_March 1.7.1 ; AS-27-2 BACnet Interface 2020 Saison 2
+    objects = chaufferie.points
+    temp = []
+    for i in objects:
+        t = str(i)
+        t = t.split(" ")
+        cmd = t[2] in ["True", "true"]
+        temp.append(int(cmd))
+    cmds.append(temp)
+    return cmds
+def get_gaz_conso(gaz):
+    test = torch.load('C:/Users/hugom/OneDrive/Documents/Stage_2025/dev_Cnn/dataset/temp.pt', weights_only=False)
+    X = test['input']
+    X = X.reshape(-1)
+    X = list(X)
+    conso = X[282]
+    conso_list = conso.item()
+    print(conso_list)
+    gaz.append(conso_list)
+    return gaz
+def get_water_conso(water):
+    test = torch.load('C:/Users/hugom/OneDrive/Documents/Stage_2025/dev_Cnn/dataset/temp.pt', weights_only=False)
+    X = test['input']
+    X = X.reshape(-1)
+    X = list(X)
+    water.append(X[261].item())
+    return water
+def get_time():
+    return datetime.datetime.now().hour
+def get_occupation(occupation_list):
+    ES_2807664 = BAC0.device('192.168.1.101', 2807664, bacnet)
+    objects = ES_2807664.points
+    for i in objects:
+        if "ES_2807664/occupation" in str(i):
+            t = str(i)
+            print(t)
+            t = t.split(" ")
+            occ = float(t[2])
+            occupation_list.append(occ)
+            break
+    return occupation_list
+def get_temp(temp_list):
+    ES_2807664 = BAC0.device('192.168.1.101', 2807664, bacnet)
+    objects = ES_2807664.points
+    for i in objects:
+        if "ES_2807664/temp_ext" in str(i):
+            t = str(i)
+            t = t.split(" ")
+            tmp = float(t[2])
+            temp_list.append(tmp)
+    return temp_list
+def degres_heure_glissants(temperatures, t_base=15.0):
+    dh_glissant = sum(t_base - t for t in temperatures)
+    return dh_glissant
+def write(val):
+    ES_2807664 = BAC0.device('192.168.1.101', 2807664, bacnet)
+    ES_2807664['degré jour']=val
+
+
+elec = []
+
+
+
+dj_list = [-20.0, -13.333333333333334, -10.0, -8.0, -6.666666666666667, -5.714285714285714, -5.0, -4.444444444444445, -4.0, -42.0]
+cmds_list = []
+water = []
+gaz = [40.62, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86]
+temp_list=[17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0, 17.0]
+occupation_list = [40.62, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86, 40.86]
+
+t = 25
+min = timedelta(days=0, hours=0, minutes=0, seconds=1)
+t1 = datetime.datetime.today()
+t1b = datetime.datetime.today().hour-1
+data = {"occupation_list":occupation_list,
+        "water consumption":water,
+        "electricity consumption":elec,
+        "gaz consumption":gaz,
+        "temp_ext":temp_list,
+        "dj":dj_list,
+        'commandes': cmds_list,
+        "time":t}
+
+occupation_list = get_occupation([])
+while True:
+    #padding d'initiation
+    if len(cmds_list)<w:
+        while (len(cmds_list)<w):
+            cmds_list = get_cmds(cmds_list)
+    if len(occupation_list)<(2*w):
+        while (len(occupation_list)<(2*w)):
+            occupation_list = get_occupation(occupation_list)
+    if len(temp_list)<w:
+        while (len(temp_list)<w):
+            temp_list = get_temp(temp_list)
+    if len(gaz)<(2*w+1):
+        while (len(gaz)<(2*w+1)):
+            gaz = get_gaz_conso(gaz)
+    if len(water)<(2*w+1):
+        while (len(water)<(2*w+1)):
+            water = get_water_conso(water)
+    if len(dj_list)<(w/2):
+        while (len(dj_list)<(w/2)):
+            dj_list.append(degres_heure_glissants(temp_list)/(len(dj_list)+1))
+    t2 = datetime.datetime.today()
+    if (t2-t1)>=min:
+        #ajout conso gaz
+        gaz = gaz[1:]
+        gaz = get_gaz_conso(gaz)
+        data['gaz consumption'] = gaz
+        # ajout conso water
+        water = water[1:]
+        water = get_water_conso(water)
+        data['water consumption'] = water
+        #ajout time
+        T = get_time()
+        data['time'] = T
+        #ajout température
+        temp_list = temp_list[1:]
+        temp_list = get_temp(temp_list)
+        data['temp_ext'] = temp_list
+        #ecriture des commandes
+        t1 = datetime.datetime.today()
+        cmds_list = cmds_list[1:]
+        cmds_list = get_cmds(cmds_list)
+        data['commandes'] = cmds_list
+        #ecriture des trends
+        occupation_list = occupation_list[1:]
+        occupation_list = get_occupation(occupation_list)
+        data['occupation_list'] = occupation_list
+        writing_trends(data)
+    t2b = datetime.datetime.today().hour
+    if t2b!=t1b:
+        print("here i m")
+        #ajout température
+        temp_list = temp_list[1:]
+        temp_list = get_temp(temp_list)
+        t1b = datetime.datetime.today().hour
+        dj = degres_heure_glissants(temp_list)
+        dj_list = dj_list[1:]
+        dj_list.append(dj)
+        print(dj_list)
+        data['dj'] = dj_list
+        writing_trends(data)
