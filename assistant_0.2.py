f0f733a98dac28351b74672ebdb381a8b354af87
zaurdar <hugo.musin@gmail.com>
2025-08-25 13:38:10 +0200

    Update assistant_0.2.py

--- assistant_0.2.py
+++ assistant_0.2.py
@@ -335,7 +335,7 @@ def _main_():
 
             except:
                 output = "room has not been recognized"
-        return output
+        return room_dic[output]
 
     def get_occupation(data=data_loaded):
         return f"occupation : {data['occupation_list'][-1]}"
